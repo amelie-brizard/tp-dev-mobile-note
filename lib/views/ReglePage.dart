ebd394b5fa79f09d241d0983a765d0cb85a80413
amelie_brizard <amelie.brizard@etu.univ-orleans.fr>
2024-03-19 17:53:34 +0100

    Modification des items de la page des règles et tentative d'ajout de l'image

--- lib/views/ReglePage.dart
+++ lib/views/ReglePage.dart
@@ -7,10 +7,21 @@ class ReglesPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Règles'),
       ),
-      body: const Center(
-        child: Text(
-            'Pour jouer, vous devez rentrer un nombre dans le but de trouver celui choisi aléatoirement. L’application vous indiquera si celui que vous avez choisi est plus petit ou plus grand que le nombre déterminé aléatoirement au préalable. À la fin de la partie, le nombre de tentatives que vous aurez effetué sera précisé et votre score sera mémorisé et consultable sur la page des scores.'),
-      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            'images/rules.jpg',
+            width: 250,
+            height: 180,
+          ),
+          const Text(
+            'Pour jouer, vous devez rentrer un nombre dans le but de trouver celui choisi aléatoirement. L’application vous indiquera si celui que vous avez choisi est plus petit ou plus grand que le nombre déterminé aléatoirement au préalable. À la fin de la partie, le nombre de tentatives que vous aurez effetué sera précisé et votre score sera mémorisé et consultable sur la page des scores.',
+            textAlign: TextAlign.center,
+          ),
+        ],
+      )),
     );
   }
 }
